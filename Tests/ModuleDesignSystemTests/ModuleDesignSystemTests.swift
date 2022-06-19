3003b4323f8dd2af389c630d5dce6667a2c03e6f
LEE ZHE YU <tzef8220@gmail.com>
2022-06-19 11:37:13 +0800

    Move codes into sources folder

--- Tests/ModuleDesignSystemTests/ModuleDesignSystemTests.swift
+++ Tests/ModuleDesignSystemTests/ModuleDesignSystemTests.swift
@@ -6,6 +6,6 @@ final class ModuleDesignSystemTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ModuleDesignSystem().text, "Hello, World!")
+        // XCTAssertEqual(ModuleDesignSystem().text, "Hello, World!")
     }
 }
